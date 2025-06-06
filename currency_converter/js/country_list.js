5763b3b585eb4cbc0bcb8851929431a30f38438d
Faithirakoze <f.irakoze2@alustudent.com>
2025-06-05 20:27:50 -0700

    Currency Converter App

---
+++ currency_converter/js/country_list.js
@@ -0,0 +1,10 @@
+let country_code = {
+        USD: "US",
+        JPY: "JP",
+        EUR: "EU",
+        RUB: "RU",
+        GBP: "GB",
+        RWF: "RW",
+        NRA: "NR",
+        SHL: "SH",
+}
